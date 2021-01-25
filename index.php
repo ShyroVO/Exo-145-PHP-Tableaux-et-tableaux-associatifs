e098ad179cedb84a29724d31e8ac1f65db2ef3ad
ShyroVO <bosquet.laurie@outlook.com>
2021-01-25 15:28:38 +0100

    1.0

--- index.php
+++ index.php
@@ -2,34 +2,40 @@
 
 // Premiere ligne
 
-$fruits = array("Apple", "Banana" "Orange");
-echo ;
+$fruits = array("Apple", "Banana","Orange");
+echo count($fruits) ;
+echo " / deuxieme entre: " . $fruits[1];
 
 //Deuxieme ligne
+$age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
 
+echo "<br><br>Ben is " . $age['Ben'] . " years old.<br><br>";
 
-$age = array("Peter" "35", "Ben" "37", "Joe" "43");
-
-echo "<br><br>Ben is " .  . " years old.<br><br>";
-
-($age  $x  $y) {
-    echo "<br>Key=" .  . ", Value=" . ;
+foreach($age as $x => $y) {
+    echo "<br>Key=" . $x . ", Value=" . $y ;
 }
 
 
 
 //Troisieme ligne
+echo "<br><br>";
 
 $colors = array("red", "green", "blue", "yellow");
-;
+asort($colors);
+foreach ($colors as $value){
+    echo "$value / ";
+}
+
+echo "<br><br>";
 
 
 // Quatrieme ligne
 
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-;
+ksort($age);
+foreach ($age as $key => $val){
+    echo "$key = $val / ";
+}
 
 
-
-
-
+?>
